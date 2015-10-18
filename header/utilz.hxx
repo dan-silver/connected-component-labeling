b812b15505ff45a4d8646822e817f8b254f5a7c9
Dan Silver <dannysilver3@gmail.com>
2015-10-17 23:37:20 -0500

    first pass at histogram equalization

--- header/utilz.hxx
+++ header/utilz.hxx
@@ -44,6 +44,7 @@ class Histogram {
             return this;
         }
 
+        // creates an HTML page with an embeded chart
         Histogram* saveHistogramToFile(string chartTitle) {
             std::ofstream out(("../public/charts/" + chartTitle + ".html").c_str());
             out << "<link href='../c3.min.css' rel='stylesheet' type='text/css'><script src='../d3.min.js'></script><script src='../c3.min.js'></script>";
@@ -53,7 +54,7 @@ class Histogram {
             out << "    bindto: '#chart',";
             out << "    data: {";
             out << "      columns: [";
-            out << "        ['Number of Pixels With Value', ";
+            out << "        ['Number of Pixels Or Probability', ";
             
             // print the data into the HTML string
             for (int i=0; i<this->number_of_bins; i++) {
@@ -61,12 +62,64 @@ class Histogram {
             }
 
             out << "]], types: {";
-            out << "'Number of Pixels With Value': 'area'";
+            out << "'Number of Pixels Or Probability': 'area'";
             out << "}}});</script>";
             out.close();
             return this;
         }
 
+
+        int* create_output_gray_map() {
+            //array index = input gray value
+            //array element value = output gray value
+            int* output_gray_map = new int[number_of_bins];
+
+            //calcuate the frequency sum
+            int frequency_sum = 0;
+            for (int i=0; i < this->number_of_bins; i++)
+                frequency_sum += this->pixel_count_array[i];
+            
+            //calculate the PDF
+            float* pdf = new float[this->number_of_bins];
+            for (int i=0; i < this->number_of_bins; i++) {
+                pdf[i] = pixel_count_array[i]/frequency_sum;
+            }
+
+            //calculate the CDF
+            float* cdf = new float[this->number_of_bins];
+            cdf[0] = 0;
+            for (int i=1; i < this->number_of_bins; i++) {
+                cdf[i] = cdf[i-1] + pdf[i];
+            }
+
+
+            // round(CDF*7)
+
+            for (int i=0; i < this->number_of_bins; i++) {
+                output_gray_map[i] = (int)(cdf[i] * 255);
+            }
+
+            return output_gray_map;
+        }
+
+        //performs histogram equalization
+        Histogram* equalize(cimg_library::CImg<> &equalized_image) {
+            equalized_image.assign(this->image.width(),this->image.height(),1,this->image.spectrum());
+
+            int* input_output_gray_map = create_output_gray_map();
+            //loop through each pixel and replace it using the map
+            int value;
+            for (int r = 0 ; r < this->image.height(); r++) {
+                for (int c= 0 ; c < this->image.width(); c++) {
+                    value = (int)image(c, r, 0, this->channel);
+                    equalized_image(c, r, 0, this->channel) = input_output_gray_map[value];
+                }
+            }
+
+            return this;
+        }
+
+        //rescale Y axis to [0, 1]
         Histogram* normalize() {
             //get the maximum count in any bin
             int max_count = -1;
@@ -92,11 +145,20 @@ void plot_histogram(cimg_library::CImg<> &image, int number_of_bins) {
         string title = os.str();
         os << " (normalized)";
         string normaled_title = os.str();
+        os << " (equalized)";
+        string equalized_title = os.str();
 
         Histogram(image, number_of_bins, channel)
             .saveHistogramToFile(title) //save charts before normalizing
-            ->normalize()
+            ->normalize() //rescale Y axis to [0, 1]
             ->saveHistogramToFile(normaled_title); //save normalized charts
+
+        // this time run histogram equalization
+        cimg_library::CImg<> equalized_image;
+        Histogram(image, number_of_bins, channel)
+            .equalize(equalized_image)
+            ->saveHistogramToFile(equalized_title); //save equalized charts
+
     }
 }
 
